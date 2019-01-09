440ecb30d1a639ee42c1875b5401e98051b39e7a
thallesvf <thallesvf@3589086.no-reply.drupal.org>
2019-01-09 16:30:50 -0200

    Issue #3017758 by thalles, dev.krishna, RenatoG: Drupal dependency injection

--- src/Controller/InviteAccessController.php
+++ src/Controller/InviteAccessController.php
@@ -5,11 +5,31 @@ namespace Drupal\invite\Controller;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines the access control handler for invite routes.
  */
 class InviteAccessController extends ControllerBase {
+
+  protected $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(CurrentRouteMatch $route_match) {
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+       $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -21,7 +41,7 @@ class InviteAccessController extends ControllerBase {
     }
     else {
       /** @var \Drupal\invite\InviteInterface $invite */
-      $invite_from_url = \Drupal::routeMatch()->getParameter('invite');
+      $invite_from_url = $this->routeMatch->getParameter('invite');
       return AccessResult::allowedIf($account->id() && $account->id() == $invite_from_url->getOwnerId() && $account->hasPermission('resend own invitations'))
         ->cachePerPermissions()
         ->cachePerUser();
@@ -38,7 +58,7 @@ class InviteAccessController extends ControllerBase {
     }
     else {
       /** @var \Drupal\invite\InviteInterface $invite */
-      $invite_from_url = \Drupal::routeMatch()->getParameter('invite');
+      $invite_from_url = $this->routeMatch->getParameter('invite');
       return AccessResult::allowedIf($account->id() && $account->id() == $invite_from_url->getOwnerId())
         ->cachePerPermissions()
         ->cachePerUser();
